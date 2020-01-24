596a3913e386bfa5e165bb2b6d2cdd38f568b863
AlbertoParente <albertoparente20@gmail.com>
2020-01-23 21:06:29 -0300

    Creating DepartmentController

---
+++ src/main/java/com/company/controller/DepartmentController.java
@@ -0,0 +1,20 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/department")
+public class DepartmentController {
+
+	@GetMapping("/register")
+	public String register() {
+		return "/department/register";
+	}
+	
+	@GetMapping("/departmentlist")
+	public String departmentlist() {
+		return "/department/list";
+	}
+}
