83262db4514fcfa92fb7f2e62a833728b283df06
AlbertoParente <albertoparente20@gmail.com>
2020-01-27 00:31:48 -0300

    Creating home page and fragmentation layout

--- src/main/java/com/company/controller/DepartmentController.java
+++ src/main/java/com/company/controller/DepartmentController.java
@@ -1,13 +1,20 @@
 package com.company.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.company.models.Department;
+import com.company.repository.DepartmentRepository;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
 
+	@Autowired
+	DepartmentRepository departmentRepository;
+	
 	@GetMapping("/register")
 	public String register() {
 		return "/department/register";
@@ -17,4 +24,13 @@ public class DepartmentController {
 	public String departmentlist() {
 		return "/department/list";
 	}
+	
+	@RequestMapping("/deletedepartment")
+	public String deletarEvento(long id){
+		Department department = departmentRepository.findById(id);
+		departmentRepository.delete(department);		
+		return "redirect:/department";
+	}
+	
+	
 }
