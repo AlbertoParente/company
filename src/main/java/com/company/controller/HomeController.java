0966219cbbb3813be1047b38578f17772897f6d2
AlbertoParente <albertoparente20@gmail.com>
2020-02-02 17:19:21 -0300

    Adjusting in project

--- src/main/java/com/company/controller/HomeController.java
+++ src/main/java/com/company/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.company.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
---
+++ src/test/java/com/companyTeste/CompanyTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
