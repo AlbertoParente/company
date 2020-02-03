528d03122e918f5a2b241de4bc29a9b59f557cff
AlbertoParente <albertoparente20@gmail.com>
2020-02-03 15:30:08 -0300

    $ creating a post (save) method in the department and adjusting the post save in page

--- src/main/java/com/company/controller/DepartmentController.java
+++ src/main/java/com/company/controller/DepartmentController.java
@@ -1,20 +1,34 @@
 package com.company.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.company.models.Department;
+import com.company.repository.DepartmentRepository;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
 	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
 	@GetMapping("/register")
-	public String departmentRegister() {
+	public String departmentRegister(Department department) {
 		return "/department/register";
 	}
 	
 	@GetMapping("/list")
 	public String departmentList() {
 		return "/department/list";
+	}
+	
+	@PostMapping("/save")
+	public String save(Department department) {
+		departmentRepository.save(department);
+		return "redirect:/department/register";
 	}
 }
