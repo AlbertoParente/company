0e7647058156a984a2420696c77912a796a4edc6
AlbertoParente <albertoparente20@gmail.com>
2020-02-05 22:55:19 -0300

    Modifying list method in OfficeController

--- src/main/java/com/company/controller/OfficeController.java
+++ src/main/java/com/company/controller/OfficeController.java
@@ -1,20 +1,31 @@
 package com.company.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.company.models.Office;
+import com.company.repository.OfficeRepository;
 
 @Controller
 @RequestMapping("/office")
 public class OfficeController {
 
+	@Autowired
+	private OfficeRepository officeRepository;
+	
 	@GetMapping("/register")
 	public String officeRegister() {
 		return "/office/register";
 	}
 	
 	@GetMapping("/list")
-	public String officeList() {
-		return "/office/list";
+	public ModelAndView officeList() {
+		ModelAndView mv = new ModelAndView("office/list");
+		Iterable<Office> office = officeRepository.findAll();
+		mv.addObject("office", office);
+		return mv;
 	}
 }
