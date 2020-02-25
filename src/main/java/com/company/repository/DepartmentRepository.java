08cf96e9cfb9616d9778e8ac977ce54ad3735ea7
AlbertoParente <albertoparente20@gmail.com>
2020-02-25 17:09:55 -0300

    Finalizing the delete and departmentContainsOffice method

--- src/main/java/com/company/repository/DepartmentRepository.java
+++ src/main/java/com/company/repository/DepartmentRepository.java
@@ -1,18 +1,10 @@
 package com.company.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.company.models.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long>{
-
+	
 	public Department findById(long id);
 	
-	public static boolean departmentContainsOffice(Long id) {
-		Department department = new Department();
-		if(department.getOffices().isEmpty()) {
-			return false;
-		}
-		return true;
-	}
 }
