1a9a63195ebeda9ac7fb9c17124ca3c63c865fb2
AlbertoParente <albertoparente20@gmail.com>
2020-02-27 23:55:44 -0300

    Creating save method in OfficeController

--- src/main/java/com/company/controller/OfficeController.java
+++ src/main/java/com/company/controller/OfficeController.java
@@ -3,10 +3,14 @@ package com.company.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.company.models.Department;
 import com.company.models.Office;
+import com.company.repository.DepartmentRepository;
 import com.company.repository.OfficeRepository;
 
 @Controller
@@ -15,6 +19,9 @@ public class OfficeController {
 
 	@Autowired
 	private OfficeRepository officeRepository;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
 	
 	@GetMapping("/register")
 	public String officeRegister() {
@@ -27,5 +34,12 @@ public class OfficeController {
 		Iterable<Office> office = officeRepository.findAll();
 		mv.addObject("office", office);
 		return mv;
+	}
+	
+	@PostMapping("/save")
+	public String save(Office office, RedirectAttributes attr) {
+		officeRepository.save(office);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+		return "redirect:/office/register";
 	}
 }
