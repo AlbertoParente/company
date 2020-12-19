7f1cc9850a48c476256e985385e1c52acf29673c
AlbertoParente <albertoparente20@gmail.com>
2020-12-18 21:11:11 -0300

    Refatoring department controller

--- src/main/java/com/albertoparente/company/web/controller/DepartmentController.java
+++ src/main/java/com/albertoparente/company/web/controller/DepartmentController.java
@@ -1,20 +1,36 @@
 package com.albertoparente.company.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.albertoparente.company.domain.Department;
+import com.albertoparente.company.service.DepartmentService;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
-
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
 	@GetMapping("/register")
-	public String departmentRegister() {
+	public String departmentRegister(Department department) {
 		return "/department/register";
 	}
 	
 	@GetMapping("/list")
-	public String departmentList() {
-		return "/department/list"; 
+	public String departmentList(ModelMap model) {
+		model.addAttribute("departments", departmentService.findAll());
+		return "/department/list";
+	}
+	
+	@PostMapping("/save")
+	public String salve(Department department) {
+		departmentService.save(department);
+		return "redirect:/department/register";
 	}
 }
