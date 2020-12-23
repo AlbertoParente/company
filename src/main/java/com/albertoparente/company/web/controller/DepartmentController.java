46225839d0d7a12c32dcfe77c069b972c07d1684
AlbertoParente <albertoparente20@gmail.com>
2020-12-22 22:49:50 -0300

    Refatoring department controller

--- src/main/java/com/albertoparente/company/web/controller/DepartmentController.java
+++ src/main/java/com/albertoparente/company/web/controller/DepartmentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.albertoparente.company.domain.Department;
 import com.albertoparente.company.service.DepartmentService;
@@ -30,27 +31,32 @@ public class DepartmentController {
 	}
 	
 	@PostMapping("/save")
-	public String salve(Department department) {
+	public String salve(Department department, RedirectAttributes attr) {
 		departmentService.save(department);
+		attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
 		return "redirect:/department/register";
 	}
 	
 	@GetMapping("/edit/{id}")
 	public String departmentBeforeEdit(@PathVariable("id") Long id, ModelMap model) {
 		model.addAttribute("department", departmentService.findById(id));
-		return "department/register";
+		return "department/edit";
 	}
 	
 	@PostMapping("/edit")
-	public String departmentEdit(Department department) {
+	public String departmentEdit(Department department, RedirectAttributes attr) {
 		departmentService.update(department);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 		return "redirect:/department/register";
 	}
 	
 	@GetMapping("/delete/{id}")
-	public String departmentDelete(@PathVariable("id") Long id, ModelMap model) {
+	public String departmentDelete(@PathVariable("id") Long id, ModelMap model, RedirectAttributes attr) {
 		if(!departmentService.departmentContainsOffice(id)) {
+			model.addAttribute("fail","Falha ao remover. Departamento possui cargo(s) vinculado(s).");			 
+		} else {
 			departmentService.delete(id);
+			attr.addFlashAttribute("success","Departamento excluído com sucesso.");	
 		}
 		return departmentList(model);
 	}
