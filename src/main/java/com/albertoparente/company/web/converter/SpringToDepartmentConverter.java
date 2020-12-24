82e2f1ef25d7280ed220b8105dfb55560da15c5b
AlbertoParente <albertoparente20@gmail.com>
2020-12-24 20:59:52 -0300

    Adding SpringToDepartmentConverter Class

---
+++ src/main/java/com/albertoparente/company/web/converter/SpringToDepartmentConverter.java
@@ -0,0 +1,25 @@
+package com.albertoparente.company.web.converter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.albertoparente.company.domain.Department;
+import com.albertoparente.company.service.DepartmentService;
+
+@Component
+public class SpringToDepartmentConverter implements Converter<String, Department> {
+
+	@Autowired
+	DepartmentService departmentService;
+	
+	@Override
+	public Department convert(String text) {
+		if(text.isEmpty()) {
+			return null;
+		}
+		Long id = Long.valueOf(text);
+		return departmentService.findById(id);
+	}
+
+}
