71cb039a34c5cb8d8296c5e97304aeada22289a1
AlbertoParente <albertoparente20@gmail.com>
2021-01-01 19:50:55 -0300

    Adding delete and edit methods in employee

--- src/main/java/com/albertoparente/company/web/controller/EmployeeController.java
+++ src/main/java/com/albertoparente/company/web/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,6 +44,18 @@ public class EmployeeController {
 		employeeService.save(employee);
 		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
 		return "redirect:/employee/register";
+	}
+	
+	@PostMapping("/edit")
+	public String employeeEdit(Employee employee, RedirectAttributes attr) {
+		employeeService.update(employee);
+		attr.addFlashAttribute("success", "Funcionario editado com sucesso.");
+		return "redirect:/employee/list";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String employeeDelete(@PathVariable("id") Long id, RedirectAttributes attr) {
+		return "redirect:/employee/list";
 	}
 	
 	@ModelAttribute("offices")
