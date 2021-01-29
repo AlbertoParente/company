056b72376ca82b223f7554341a58bfcbc9b525d1
AlbertoParente <albertoparente20@gmail.com>
2021-01-28 22:11:02 -0300

    Create count method

--- src/main/java/com/albertoparente/company/dao/OfficeDaoImpl.java
+++ src/main/java/com/albertoparente/company/dao/OfficeDaoImpl.java
@@ -1,9 +1,33 @@
 package com.albertoparente.company.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import com.albertoparente.company.domain.Office;
+import com.albertoparente.company.util.Pagination;
 
 @Repository
 public class OfficeDaoImpl extends AbstractDao<Office, Long> implements OfficeDao {
 
+	public Pagination<Office> searchPaged(int page) {
+		int size = 5;
+		int start = (page - 1) * size;
+		
+		List<Office> office = getEntityManager()
+				.createQuery("select o from Office o oreder by o.name asc", Office.class)
+				.setFirstResult(start)
+				.setMaxResults(size)
+				.getResultList();
+		
+		long totalRecords = count();
+		long totalPages = (totalRecords + (size - 1)) / size;
+		
+		return new Pagination<>(size, page, totalPages, cargos);
+	}
+	
+	public long count() {
+		return getEntityManager()
+				.createNamedQuery("select count(*) from Office", Long.class)
+				.getSingleResult();
+	}
 }
